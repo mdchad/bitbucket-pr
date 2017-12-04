6ea7ec29d2b67e14e0c2bff368d848e69c83c355
Irsyad <irsyad.muhd@gmail.com>
2017-12-04 12:19:08 +0800

    add repo url

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const branch = require('git-branch');
+const open = require('opn');
+const gitRemoteOriginUrl = require('git-remote-origin-url');
+
+gitRemoteOriginUrl().then(url => {
+    // for bitbucket 
+    // url = https://username@bitbucket.org/team/repositry.it
+    let removeGit,
+        splitChar,
+        i,
+        removeUsername,
+        uri
+    removeGit = url.slice(0, -4)
+    splitChar = sl.split('@')
+    i = (ssl[0].indexOf('/')) + 2
+    removeUsername = ssl[0].substr(0, i)
+    ssl[0] = removeUsername
+    uri = ssl.join("")
+    open(uri + '/pull-requests/new?source=' + branch.sync() + '&t=1')
+    process.exit()
+});

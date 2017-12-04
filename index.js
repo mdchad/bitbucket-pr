451a94b1ba92ca44d3999ca92aeebfd8c312735f
Irsyad <irsyad.muhd@gmail.com>
2017-12-04 17:52:50 +0800

    add comment and remove console

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const open = require('opn');
 const gitRemoteOriginUrl = require('git-remote-origin-url');
 
 gitRemoteOriginUrl().then(url => {
-    // for bitbucket 
-    // url = https://username@bitbucket.org/team/repositry.it
+    // open pull-request window in default browser for bitbucket 
+    // url = https://username@bitbucket.org/team/repositry.git
     let removeGit,
         splitChar,
         i,
         removeUsername,
         uri
+
     removeGit = url.slice(0, -4)
-    splitChar = sl.split('@')
-    i = (ssl[0].indexOf('/')) + 2
-    removeUsername = ssl[0].substr(0, i)
-    ssl[0] = removeUsername
-    uri = ssl.join("")
-    open(uri + '/pull-requests/new?source=' + branch.sync() + '&t=1')
+    splitChar = removeGit.split('@')
+    i = (splitChar[0].indexOf('/')) + 2
+    removeUsername = splitChar[0].substr(0, i)
+    splitChar[0] = removeUsername
+    uri = splitChar.join("")
+    open(`${uri}/pull-requests/new?source=${branch.sync()}&t=1`)
     process.exit()
 });

3094a2638ce538abc474d5883d61615a06c356fb
Irsyad <irsyad.muhd@gmail.com>
2017-12-07 14:48:52 +0800

    change to using node url

--- index.js
+++ index.js
@@ -2,22 +2,16 @@
 const branch = require('git-branch');
 const open = require('opn');
 const gitRemoteOriginUrl = require('git-remote-origin-url');
+const { URL } = require('url');
 
 gitRemoteOriginUrl().then(url => {
     // open pull-request window in default browser for bitbucket 
     // url = https://username@bitbucket.org/team/repositry.git
-    let removeGit,
-        splitChar,
-        i,
-        removeUsername,
-        uri
+    const address = new URL(url)
+    let uri
 
-    removeGit = url.slice(0, -4)
-    splitChar = removeGit.split('@')
-    i = (splitChar[0].indexOf('/')) + 2
-    removeUsername = splitChar[0].substr(0, i)
-    splitChar[0] = removeUsername
-    uri = splitChar.join("")
+    // remove git
+    uri = address.origin + address.pathname.slice(0, -4)
     open(`${uri}/pull-requests/new?source=${branch.sync()}&t=1`)
     process.exit()
 });
